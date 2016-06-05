693b0e4ea2055ea6639458e5c122f1f683c7ecad
snorr <snorredc@gmail.com>
2016-06-05 21:39:05 +0200

    Make movement smooth

--- src/unit_mover.rs
+++ src/unit_mover.rs
@@ -1,6 +1,7 @@
 use std::time::{Duration, Instant};
 
 use glorious::{Behavior, Renderer};
+use sdl2::rect::Rect;
 
 use common::{Message, State};
 use unit::Unit;
@@ -14,6 +15,7 @@ pub struct UnitMover {
     origin: (u32, u32),
     path: Vec<(u32, u32)>,
     index: usize,
+    delta: f32,
     start: Option<Instant>,
 }
 
@@ -29,7 +31,19 @@ impl UnitMover {
             origin: origin,
             path: path,
             index: 0,
+            delta: 0.0,
             start: None,
+        }
+    }
+
+    fn current(&self) -> ((u32, u32), (u32, u32)) {
+        if self.path.is_empty() {
+            return (self.origin, self.origin);
+        }
+        if self.index == 0 {
+            (self.origin, self.path[0])
+        } else {
+            (self.path[self.index - 1], self.path[self.index])
         }
     }
 }
@@ -47,7 +61,8 @@ impl<'a> Behavior<State<'a>> for UnitMover {
             Some(start) => start,
         };
         let elapsed = now.duration_since(start);
-        let i = as_millis(elapsed) / MOVE_TILE_MS;
+        let ms = as_millis(elapsed);
+        let i = ms / MOVE_TILE_MS;
 
         if i >= self.path.len() as u64 {
             let unit = self.unit.take().expect("missing unit");
@@ -57,14 +72,27 @@ impl<'a> Behavior<State<'a>> for UnitMover {
             queue.push(Message::UnitMoved(self.origin, to));
         } else {
             self.index = i as usize;
+            self.delta = (ms % MOVE_TILE_MS) as f32 / MOVE_TILE_MS as f32;
         }
     }
 
     fn render(&mut self, state: &State<'a>, renderer: &mut Renderer) {
         if let Some(ref unit) = self.unit {
-            let pos = self.path[self.index];
-            let rect = state.tile_rect(pos);
+            let (from, to) = self.current();
+            let rect_a = state.tile_rect(from);
+            let rect_b = state.tile_rect(to);
+            let (w, h) = state.tile_size;
+            let a = (rect_a.x(), rect_a.y());
+            let b = (rect_b.x(), rect_b.y());
+            let (x, y) = lerp(a, b, self.delta);
+            let rect = Rect::new(x, y, w, h);
             render_unit(unit, rect, true, state, renderer);
         }
     }
+}
+
+fn lerp(a: (i32, i32), b: (i32, i32), v: f32) -> (i32, i32) {
+    let x = a.0 + ((b.0 - a.0) as f32 * v) as i32;
+    let y = a.1 + ((b.1 - a.1) as f32 * v) as i32;
+    (x, y)
 }
